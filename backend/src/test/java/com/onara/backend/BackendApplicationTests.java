b306d76d21bb4c34d81134cf9455393b0f4345f3
Khalid <kibwalid@gmail.com>
2024-07-12 21:50:48 +0600

    Added initial spring backend

---
+++ backend/src/test/java/com/onara/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onara.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
